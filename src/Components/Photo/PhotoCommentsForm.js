8d9a3eac1648ddde056bc8e02cc8834f3774df15
Davi Firmino <dafirsouza@gmail.com>
2020-08-13 22:47:42 -0300

    inicio modal de fotos

---
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import {ReactComponent as Enviar} from '../../Assets/enviar.svg'
+import useFetch from '../../Hooks/useFetch'
+import {COMMENT_POST} from '../../api'
+import Error from '../Helper/Error'
+import styles from './PhotoCommentsForm.module.css'
+
+const PhotoCommentsForm = ({id, setComments}) => {
+    const [comment, setComment] = useState(null)
+    const { error, request } = useFetch()
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const { url, options } = COMMENT_POST(id, { comment })
+        const {response, json} = await request(url, options)
+        if(response.ok) {
+            setComment('')
+            setComments((comments) => [...comments, json])
+        }
+    }
+    
+    const handleChange = ({target}) => {
+        setComment(target.value)
+    }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+            <textarea  value={comment} onChange={handleChange} id='comment' name='comment' placeholder='Comente...'/>
+            <button> <Enviar /> </button>
+            {error && <Error error={error} /> }
+        </form>
+    )
+}
+
+export default PhotoCommentsForm
